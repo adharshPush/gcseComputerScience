78c6f94bcdb001b04825555058970f8a4ca2c862
adharshPush <141495740+adharshPush@users.noreply.github.com>
2024-01-30 16:14:46 +0000

    Try except

--- LBOA/challenge1.py
+++ LBOA/challenge1.py
@@ -7,8 +7,35 @@ return highest number
 '''
 
 def highest_number(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        print(num1)
-    else:
-        print("not number 1 mhwahhhhahahahahah")
-highest_number(1,2,3)
+    try:
+        num1=int(num1)
+        num2=int(num2)
+        num3=int(num3)
+
+        if num1 >= num2 and num1 >= num3:
+            return num1
+        elif num2 >= num1 and num2 >= num3:
+            return num2
+        else:
+            return num3
+    except:
+        return "You haven't entered numbers for each comparision"
+      
+# test your program with different data 
+    
+# valid
+print(highest_number(1, 2, 3))
+print(highest_number(3, 2, 1))
+print(highest_number(2, 3, 1))
+print(highest_number(3, 3, 1))
+print(highest_number(3, 3, 3))
+    
+# border
+print(highest_number(1, "2", "3"))
+
+# border
+print(highest_number("1", "2", "3"))
+
+# erroneous
+print(highest_number(1, "two", "3"))
+                                                                                                                                                                                                                                                                        

6001a3196970b287147caaa41986db19fd7e5697
adharshPush <141495740+adharshPush@users.noreply.github.com>
2024-01-30 15:48:08 +0000

    highest number

---
+++ LBOA/challenge1.py
@@ -0,0 +1,14 @@
+'''
+subprogram has 3 parmaters
+num1, num2, num3
+should take in 3 numbers
+return highest number
+
+'''
+
+def highest_number(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        print(num1)
+    else:
+        print("not number 1 mhwahhhhahahahahah")
+highest_number(1,2,3)
